9a2b864c9b74ff0bb60048c8a83740f26acb0937
Ahmed Aoulad Aamar <aouladoumarahmed100@gmail.com>
2022-02-26 18:28:39 +0100

    add date

--- ahmed.sql
+++ ahmed.sql
@@ -1,31 +1,7 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Feb 21, 2022 at 11:31 AM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 8.1.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `ahmed`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `employees`
---
 
 CREATE TABLE `employees` (
   `id` int(11) NOT NULL,
@@ -38,35 +14,18 @@ CREATE TABLE `employees` (
   `photo` varchar(500) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `employees`
---
-
 INSERT INTO `employees` (`id`, `first_name`, `last_name`, `date_of_birth`, `department`, `salary`, `function`, `photo`) VALUES
-(52, 'trump', 'dounl', '2022-02-10', 'president t', '100000', 'P', 'https://www.theglobeandmail.com/resizer/hNVMjr8hDGdPi6smvp-zWJbX4mI=/1200x0/filters:quality(80):format(webp)/arc-anglerfish-tgam-prod-tgam.s3.amazonaws.com/public/5HSZVXDII5BRRHH4S6KE4WZ7RE.jpg'),
-(53, 'AHMED', 'AOULAD', '2022-02-05', 'WE', '1300', 'Wwb dev', 'https://raw.githubusercontent.com/code-brief/img/main/20200610_212959.jpg');
+(87, 'ahmed ', 'AOULAD OUMAR', '1993-10-26', '#AIEN', '3000000', 'WEBDEVLOPER', 'https://raw.githubusercontent.com/code-brief/img/main/20200610_212959.jpg'),
+(88, 'trump', 'dounl', '2022-02-03', 'president ', '70000', 'president ', 'https://www.theglobeandmail.com/resizer/hNVMjr8hDGdPi6smvp-zWJbX4mI=/1200x0/filters:quality(80):format(webp)/arc-anglerfish-tgam-prod-tgam.s3.amazonaws.com/public/5HSZVXDII5BRRHH4S6KE4WZ7RE.jpg'),
+(93, 'Elon', 'mask', '2009-06-02', 'biasness', '10000000', 'conpany owner', 'https://cdn.vox-cdn.com/thumbor/RBwA33BWcDfnGZpubWFZLLb7hsA=/0x0:3926x3123/1220x813/filters:focal(1649x1248:2277x1876):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/70305223/1234657391.0.jpg'),
+(94, 'aziz', 'akhanockh', '1999-02-02', 'prisdantdz', '1000', 'r', 'https://static.lematin.ma/files/lematin/images/articles/2021/10/15f905c648ac6c3fc451e5e4ede5d466.jpg'),
+(95, 'STIVE', 'jobe', '0034-03-05', 'pre', '5000000', 'sdfg', 'https://s.yimg.com/ny/api/res/1.2/AX5H0MqDU7xaHyAx2lSwRA--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTU4Mw--/https://s.yimg.com/uu/api/res/1.2/9j3ZjrbTmUxyQk.Ws.SYPw--~B/aD00NTA7dz00OTQ7YXBwaWQ9eXRhY2h5b24-/https://www.blogcdn.com/www.engadget.com/media/2011/10/steve-jobs.jpg'),
+(96, 'mohamed', 'salah', '1999-05-06', 'asdf', '10000', 'player', 'https://img.a.transfermarkt.technology/portrait/header/148455-1546611604.jpg'),
+(97, 'miss', 'lion', '1800-12-01', 'rdf', '300000', 'player', 'https://i.skyrock.net/3056/39483056/pics/1732872664.jpg');
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `employees`
---
 ALTER TABLE `employees`
   ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `employees`
---
+  
 ALTER TABLE `employees`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=98;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
